5fc39a49fa36643ac1ad85270cd8b186394005ab
Sig <33547382+sigs1gg@users.noreply.github.com>
2021-01-17 22:19:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 import os
 import traceback
-import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,22 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
-client = discord.Client() 
-@client.event
-    async def on_voice_state_update(before, after): 
-    if after.server.id == '653523307232231425': 
-        nowtime = datetime.datetime.utcnow() 
-        nowtime = nowtime + datetime.timedelta(hours=9) 
-        nowtime = nowtime.strftime("%m/%d-%H:%M") 
-        vcchannel = client.get_channel('653523307865309187') 
- 
-        if(before.voice_channel is None): 
-            jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
-            await client.send_message(vcchannel, jointext) 
-        elif(after.voice_channel is None): 
-            outtext=nowtime + "に　"+ before.name + "　が　"+ before.voice_channel.name + " から退出しました。" 
-            await client.send_message(vcchannel, outtext) 
 
 
 @bot.command()
